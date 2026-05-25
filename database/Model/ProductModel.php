86de0832249bc85608e7f8050701da5d47434d0d
Dragon <eric77251@gmail.com>
2026-05-25 09:50:28 -0500

    programa

---
+++ database/Model/ProductModel.php
@@ -0,0 +1,11 @@
+<?php
+namespace Database\Model;
+
+class ProductModel{
+    public function getId():Int{
+
+    return 123;
+    }
+}
+
+?>
---
+++ prueba.php
@@ -0,0 +1,18 @@
+<?php 
+
+require ("app/User.php");
+require("database/Model/ProductModel.php");
+require "vendor/autoload.php";
+
+use App\User;
+use Database\Model\ProductModel;
+
+$user = new App\User();
+echo $user->getName();
+
+echo "\n";
+
+$product = new ProductModel();
+echo $product->getId();
+
+?>
